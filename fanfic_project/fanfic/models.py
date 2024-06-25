9263975cf88f57063dbecaf5346c5170c7ac44f2
Avdey Tolstov <avdey.tolstov@saritasa.com>
2024-06-25 22:10:39 +0500

    Update design

--- fanfic_project/fanfic/models.py
+++ fanfic_project/fanfic/models.py
@@ -6,7 +6,7 @@ class Fanfic(models.Model):
     description = models.CharField(max_length=400)
     content = models.TextField(default='Текст фанфика будет здесь.')
     rating = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='fanfic_images/', blank=True, null=True)
+    image = models.ImageField(upload_to='fanfic_images/')
     tags = models.CharField(default='Гарем, Юри', max_length=400)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     
